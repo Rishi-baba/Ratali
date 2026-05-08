216e05eece077cde05672c413c6aef0a4a6a3cec
risibaba <rishibankar16@gmail.com>
2026-05-09 04:57:07 +0530

    auth done

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+   {
+    username: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    bamboo: {
+      type: Number,
+      default: 0,
+    },
+
+    streak: {
+      type: Number,
+      default: 0,
+    },
+
+    pandaMood: {
+      type: String,
+      default: "happy",
+    },
+
+    pandaHunger: {
+      type: Number,
+      default: 100,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
